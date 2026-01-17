63c0e1f5c840b6cd0d151764978e3f81c8597f71
Pothireddy Gelivi <pothireddy@example.com>
2026-01-17 10:50:01 +0530

    fix animation.css

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './styles/animations.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
